a2b9bd7e23ce8b16386d66b9604829157b13198a
ronalyulyanto <ronal.belajar.mengajar@gmail.com>
2024-01-23 10:44:17 +0000

    chore: First Code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          display: ["Montserrat","sans-serif"],
+          sans: ["Josefin Sans","sans-serif"],
+        }
+      },
+    },
+    plugins: [],
+  }
